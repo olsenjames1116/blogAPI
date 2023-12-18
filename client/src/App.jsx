ae7a9e5b231d1630fac3cf385aa68fe293ac169e
James Olsen <olsenjames1116@gmail.com>
2023-12-18 18:05:26 -0500

    Create a footer module

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Router from './Router';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
 	return (
 		<>
 			<Header />
 			<Router />
+			<Footer />
 		</>
 	);
 }
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<footer>
+			<div className="authorCredit">
+				Built and designed by{' '}
+				<a href="https://github.com/olsenjames1116">olsenjames1116</a>
+			</div>
+			<div className="sources">Sources</div>
+		</footer>
+	);
+}
+
+export default Footer;
