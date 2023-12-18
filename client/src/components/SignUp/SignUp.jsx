f158d77732dfe9e4837950d9b268723590688df1
James Olsen <olsenjames1116@gmail.com>
2023-12-17 22:07:47 -0500

    Create form for sign up

--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 function SignUp() {
-	return <div>SignUp</div>;
+	return (
+		<main className="signUp">
+			<form method="POST" action="/api/user/sign-up">
+				<div>
+					<label htmlFor="username">Username:</label>
+					<input id="username" name="username" type="text" />
+				</div>
+				<div>
+					<label htmlFor="password">Password:</label>
+					<input id="password" name="password" type="password" />
+				</div>
+				<div>
+					<label htmlFor="confirmPassword">Confirm Password:</label>
+					<input id="confirmPassword" name="confirmPassword" type="password" />
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+		</main>
+	);
 }
 
 export default SignUp;
