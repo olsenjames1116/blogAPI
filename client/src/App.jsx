05850ca8e8e6281d05772d3d73770fed0d23ce0f
James Olsen <olsenjames1116@gmail.com>
2023-12-18 16:54:51 -0500

    Create a header module

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Router from './Router';
+import Header from './components/Header/Header';
 
 function App() {
-	return <Router />;
+	return (
+		<>
+			<Header />
+			<Router />
+		</>
+	);
 }
 
 export default App;
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+	return (
+		<header>
+			<nav>
+				<div className="logo">Pub Chair Sports</div>
+				<ul className="links">
+					<li>
+						<a href="/sign-up">Sign Up</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+}
+
+export default Header;
