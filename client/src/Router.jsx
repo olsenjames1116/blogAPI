c9bc8293cf476ec6931d4948fc03002b73859d4c
James Olsen <olsenjames1116@gmail.com>
2023-12-17 20:56:26 -0500

    Create sign up component

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './components/Home/Home';
+import SignUp from './components/SignUp/SignUp';
 
 function Router() {
 	const router = createBrowserRouter([
 		{
 			path: '/',
 			element: <Home />,
+		},
+		{
+			path: 'sign-up',
+			element: <SignUp />,
 		},
 	]);
 
