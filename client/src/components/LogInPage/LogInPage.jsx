502db2bbf52554da9eebc0c3d4cf82a04655b9c7
James Olsen <olsenjames1116@gmail.com>
2023-12-18 18:57:54 -0500

    Create log in page component

---
+++ client/src/components/LogInPage/LogInPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LogInPage() {
+	return (
+		<main className="logIn">
+			<div className="content">
+				<h2>Log In</h2>
+			</div>
+		</main>
+	);
+}
+
+export default LogInPage;
