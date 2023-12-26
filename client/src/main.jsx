95e204e0377b2fced5c56ee97fcd473b492949ac
James Olsen <olsenjames1116@gmail.com>
2023-12-26 14:22:42 -0500

    Add style to main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
+import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
