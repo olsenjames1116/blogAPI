3c9145ba231ccf56095b5f340a67c5ed58a500bc
James Olsen <olsenjames1116@gmail.com>
2023-12-26 14:45:04 -0500

    Style and format logo

--- client/src/assets/images/index.js
+++ client/src/assets/images/index.js
@@ -1,5 +1,6 @@
 import commentImage from './comment.png';
 import userImage from './user.png';
 import loadingImage from './loading.png';
+import barstoolImage from './barstool.png';
 
-export { commentImage, userImage, loadingImage };
+export { commentImage, userImage, loadingImage, barstoolImage };
--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Logo.module.css';
+import { barstoolImage } from '../../assets/images';
 
 // Represents the logo at the top of the page.
 function Logo() {
 	return (
-		<div className="logo">
-			<Link to="/">Pub Chair Sports</Link>
+		<div className={styles.logo}>
+			<Link to="/">
+				<img src={barstoolImage} />
+				<span>Pub Chair Sports</span>
+			</Link>
 		</div>
 	);
 }
