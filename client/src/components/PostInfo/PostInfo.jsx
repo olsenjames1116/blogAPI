064f5fe6bfb868149e5ef9377f792ec2e2fa739b
James Olsen <olsenjames1116@gmail.com>
2023-12-26 19:20:08 -0500

    Style and format post info in main

--- client/src/components/PostInfo/PostInfo.jsx
+++ client/src/components/PostInfo/PostInfo.jsx
@@ -3,23 +3,26 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { commentImage } from '../../assets/images';
 import { DateTime } from 'luxon';
+import styles from './PostInfo.module.css';
 
 // Represents a post on the home page or admin dashboard with the major details of a post.
 function PostInfo({ post }) {
 	return (
-		<div className="postInfo">
+		<div className={styles.postInfo}>
 			<Link to={`/post/${post._id}`}>
-				<div>
-					<img src={post.image.file} />
+				<div className={styles.imageContainer}>
+					<img className={styles.postImage} src={post.image.file} />
 				</div>
-				<h3>{post.title}</h3>
-				<span>{post.user.username}</span>
-				<span>
-					{DateTime.fromISO(post.timestamp).toLocaleString(DateTime.DATE_MED)}
-				</span>
-				<div>
-					<img src={commentImage} />
-					<span>{post.comments ? post.comments.length : 0}</span>
+				<h3 className={styles.title}>{post.title}</h3>
+				<div className={styles.infoContainer}>
+					<span className={styles.username}>{post.user.username}</span>
+					<span className={styles.date}>
+						{DateTime.fromISO(post.timestamp).toLocaleString(DateTime.DATE_MED)}
+					</span>
+					<div className={styles.comments}>
+						<img src={commentImage} />
+						<span>{post.comments ? post.comments.length : 0}</span>
+					</div>
 				</div>
 			</Link>
 		</div>
