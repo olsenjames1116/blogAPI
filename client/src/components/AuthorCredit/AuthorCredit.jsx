44df556ded28b2ce59f657f088ae5abfbf4f5b71
James Olsen <olsenjames1116@gmail.com>
2023-12-26 21:44:42 -0500

    Style and format footer

--- client/src/components/AuthorCredit/AuthorCredit.jsx
+++ client/src/components/AuthorCredit/AuthorCredit.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './AuthorCredit.module.css';
 
 // Represents the credit to the developer.
 function AuthorCredit() {
 	return (
-		<div className="authorCredit">
+		<div className={styles.authorCredit}>
 			Built and designed by{' '}
 			<a href="https://github.com/olsenjames1116">olsenjames1116</a>
 		</div>
--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AuthorCredit from '../AuthorCredit/AuthorCredit';
 import Sources from '../Sources/Sources';
+import './Footer.module.css';
 
 // Represents the footer at the bottom of the page.
 function Footer() {
