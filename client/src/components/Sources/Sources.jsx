25d6e2262b94b82aaed904a47e7db788326519bb
James Olsen <olsenjames1116@gmail.com>
2023-12-27 13:23:46 -0500

    Style and format sources

--- client/src/components/Sources/Sources.jsx
+++ client/src/components/Sources/Sources.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import styles from './Sources.module.css';
 
 // Represents the sources in the footer of the page.
 function Sources() {
-	return <div className="sources">Sources</div>;
+	return (
+		<div className={styles.sources}>
+			Images provided by <a href="https://icons8.com/icons">Icons8</a>
+		</div>
+	);
 }
 
 export default Sources;
