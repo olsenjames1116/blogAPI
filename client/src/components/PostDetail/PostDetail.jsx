ce6d7cd790dd74c214f235743b5c1ada1af7957f
James Olsen <olsenjames1116@gmail.com>
2023-12-26 20:24:06 -0500

    Style and format post details

--- client/src/components/PostDetail/PostDetail.jsx
+++ client/src/components/PostDetail/PostDetail.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { decode } from 'html-entities';
 import parse from 'html-react-parser';
+import styles from './PostDetail.module.css';
 
 // Represents the details of a post stored in the db.
 function PostDetail({ post }) {
 	return (
-		<div className="postDetail">
-			<div>
+		<div className={styles.detail}>
+			<h2 className={styles.title}>{post.title}</h2>
+			<div className={styles.imageContainer}>
 				<img src={post.image.file} />
 			</div>
-			<h2>{post.title}</h2>
 			{parse(decode(post.text))}
 		</div>
 	);
